17dd40498566292d31aa7ac020047fd9808e931a
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-02-23 13:07:22 -0600

    Version 1.0.0 Release

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@
 -- SimpleDevelopments Discord: https://discord.gg/RWQNhKxfC4
 
 author "Andyyy#7666, N1K0#0001, SimpleDevelopments"
-description "ND Chat commands"
-version "2.1.2"
+description "ND Core Chat commands"
+version "1.0.0"
 
 fx_version "cerulean"
 game "gta5"
@@ -11,7 +11,6 @@ lua54 "yes"
 
 shared_scripts {
   "@ND_Core/init.lua",
-  "Config.lua"
 }
 
 client_scripts {
@@ -19,7 +18,8 @@ client_scripts {
 }
 
 server_scripts {
-  'server/server.lua'
+  'server/server.lua',
+  'server/versionChecker.lua'
 }
 
 dependency "ND_Core"
